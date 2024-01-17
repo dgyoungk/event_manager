3e6627631539a54106942568b7a0fe409b162a97
Daniel <150309766+dgyoungk@users.noreply.github.com>
2024-01-16 19:45:04 -0800

    Add logic for finding target hour and target day

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 
 
@@ -32,6 +33,41 @@ def clean_phone_number(phone)
   phone.insert(7, '-')
 end
 
+def get_register_time(dates)
+  dates.gsub!('/', '-').insert(dates.rindex('-') + 1, '20')
+  datetime = Time.strptime(dates, '%m-%d-%Y %k:%M')
+end
+
+def days_of_week()
+  week = {
+    Sunday: 0,
+    Monday: 1,
+    Tuesday: 2,
+    Wednesday: 3,
+    Thursday: 4,
+    Friday: 5,
+    Saturday: 6
+  }
+end
+
+def display_target_times(hour_counts)
+  puts %(Hours of the day to target:)
+  hour_counts.keys.each do |hour|
+    if hour_counts[hour] == hour_counts.values.max
+      if hour > 12
+        puts %(#{hour - 12}pm)
+      else
+        puts %(#{hour}am)
+      end
+    end
+  end
+end
+
+def display_target_day(day_counts)
+  puts %(Day of the week to target:)
+  day_counts.keys.each { |day, count| puts day if day_counts[day] == day_counts.values.max }
+end
+
 def legislators_by_zipcode(zip)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
@@ -60,7 +96,6 @@ end
 
 puts %(Event Manager Initialized!)
 
-
 contents = CSV.open(
   'event_attendees.csv',
   headers: true,
@@ -69,6 +104,9 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
+
+hour_counts = Hash.new(0)
+day_counts = Hash.new(0)
 
 contents.each do |row|
   # getting all the attributes needed for form_letter file creation from the CSV file
@@ -80,11 +118,15 @@ contents.each do |row|
   # Assignment: Clean phone numbers
   phone = clean_phone_number(row[:homephone])
 
+  date = get_register_time(row[:regdate])
+
   # Assignment: Time targeting
-  # note: the date in the CSV file is in the format of MM/DD/YY
-  # TODO:
-  # - use the registration date and time to find out what the peak registration hours are
-  # https://www.theodinproject.com/lessons/ruby-event-manager#assignment-time-targeting
+  hour_counts[date.hour] += 1
+
+  # Assignment: Day of the week targeting
+  # find out which day of the week has the most registrations
+  day_counts[days_of_week().key(date.wday)] += 1
+
 
   # getting the information of gov representatives from the CivicInfo API
   legislators = legislators_by_zipcode(zipcode)
@@ -94,4 +136,9 @@ contents.each do |row|
 
   # save_thank_you_letter(id, form_letter)
 
+  # display the target hours and target day
+  if contents.eof?
+    display_target_times(hour_counts)
+    display_target_day(day_counts)
+  end
 end
